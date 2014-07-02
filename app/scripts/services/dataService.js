1ada8c16a8364e65f509ccd960120434b6f5a642
Walter <wbrand@gmail.com>
2014-07-02 10:22:16 +0200

    service working

--- app/scripts/services/dataService.js
+++ app/scripts/services/dataService.js
@@ -1,16 +1,21 @@
 'use strict';
 
 angular.module('uicomponents')
-  .factory('dataService', function () {
-    // Service logic
-    // ...
+    .factory('dataService', function () {
 
-    var meaningOfLife = 42;
+        var data = null;
 
-    // Public API here
-    return {
-      someMethod: function () {
-        return meaningOfLife;
-      }
-    };
-  });
+        function getData(){
+            return data;
+        }
+
+        function setData(_data_) {
+            data = _data_;
+        }
+
+        // Public API here
+        return {
+            getData : getData,
+            setData : setData
+        };
+    });
--- test/spec/services/dataService.js
+++ test/spec/services/dataService.js
@@ -2,17 +2,26 @@
 
 describe('Service: Dataservice', function () {
 
-  // load the service's module
-  beforeEach(module('uicomponents'));
+    // load the service's module
+    beforeEach(module('uicomponents'));
 
-  // instantiate service
-  var dataService;
-  beforeEach(inject(function (_dataService_) {
-    dataService = _dataService_;
-  }));
+    // instantiate service
+    var dataService;
+    beforeEach(inject(function (_dataService_) {
+        dataService = _dataService_;
+    }));
 
-  it('should do something', function () {
-    expect(!!dataService).toBe(true);
-  });
+    it('should return null when getData is called', function () {
+        expect(dataService.getData()).toBeNull();
+    });
+
+    it('should keep the data when setData is called and replace all of it when called again', function(){
+        dataService.setData({name:'Walter'});
+        expect(dataService.getData()).toEqual({name:'Walter'});
+
+        // replacing the data with something else
+        dataService.setData({amount:{currency:'EUR', value: 100.00}});
+        expect(dataService.getData()).toEqual({amount:{currency:'EUR', value: 100.00}});
+    });
 
 });
