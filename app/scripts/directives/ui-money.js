e08a23a49765f0bce507e1da21674014e6ff6ec4
Walter <wbrand@gmail.com>
2014-07-02 12:53:22 +0200

    ui money

--- app/scripts/directives/ui-money.js
+++ app/scripts/directives/ui-money.js
@@ -5,21 +5,17 @@ angular.module('uicomponents')
         return {
             templateUrl: 'partials/uimoney.html',
             restrict: 'E',
-            require : 'ngModel',
+            // TODO: zorg ervoor dat je een referentie krijgt naar de controller van ngModel
             replace: true,
             scope: {
-                ngModel : '='
+                // TODO: maak een two way binding naar de waarde die in ngModel staat
             },
             link: function postLink(scope, element, attrs, ngModel) {
-                var amountParts = scope.ngModel.split('.');
-                scope.data = {
-                    euros : amountParts[0],
-                    cents : amountParts[1]
-                }
+                // TODO: lees de waarde van de ngModel van de scope, splits deze in euro's en centen en voeg deze toe aan de scope
 
                 scope.update = function(){
-                    scope.ngModel = scope.data.euros + '.' + scope.data.cents
-                    ngModel.$setValidity('isNumber', !isNaN(scope.data.euros) && !isNaN(scope.data.cents))
+                    //TODO: update de waarde van ngModel waarde in de scope
+                    //TODO: pas de validity aan, waarbij alleen getallen worden geaccepteerd
                 }
             }
         };
